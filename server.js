8b0d9acc0b780dc2d85a44394424c4d1de126559
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-19 10:23:38 -0400

    ADD KNEXFILE

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const studentsRoutes = require('./students/studentsRoutes');
 const cohortsRoutes = require('./cohorts/cohortsRoutes');
 server.use(express.json());
 
-server.use('/api/cohorts', cohortsRoutes);
-server.use('/api/students', studentsRoutes);
+server.use('/api/cohorts/', cohortsRoutes);
+server.use('/api/students/', studentsRoutes);
 
 
 module.exports = server;
