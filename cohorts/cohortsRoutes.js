437addc476109776701cde91b85922944f0c2cc5
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-19 21:22:27 -0400

    add getbyid route

--- cohorts/cohortsRoutes.js
+++ cohorts/cohortsRoutes.js
@@ -1,10 +1,44 @@
 const express = require('express');
+const cohortsDB = require('../data/helpers/cohortsDB');
 
 const router = express.Router();
 
 router.use(express.json());
 
-router.use('/', (req, res) => {
-    res.status(200).json('Cohorts rules!')
+router.get('/', async (req, res) => {
+
+    try {
+        const cohorts = await cohortsDB.get();
+        
+        if (cohorts) {
+            res.status(200).json(cohorts)
+        }
+        else {
+            res.status(404).json('There are not cohorts available.')
+        }
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
 })
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    try {
+        const cohort = await cohortsDB.getById(id);
+        
+        if (cohort) {
+            res.status(200).json(cohort)
+        }
+        else {
+            res.status(404).json('There are not cohorts available.')
+        }
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
+})
+
+
 module.exports = router;
--- students/studentsRoutes.js
+++ students/studentsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+// const db = require(db)
 const router = express.Router();
 router.use(express.json());
 
 router.use('/', (req, res) => {
     res.status(200).json('Students rules!')
 })
+
 
 module.exports = router;
